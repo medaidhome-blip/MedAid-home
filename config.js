e6bcb4e79b9f8f2459e51723e655b20ac9fdb4e9
medaidhome-blip <medaidhome@gmail.com>
2026-08-24 23:52:54 +0530

    Add Supabase URL and publishable key to config

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-sb_publishable_qTX-ExD5fIx3aT700mCMFQ_XK0mCqCb
+const SUPABASE_URL = "https://wifahwpndjvfzfdxpkup.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_qTX-ExD5fIx3aT700mCMFQ_XK0mCqCb";
